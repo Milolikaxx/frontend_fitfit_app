7ca788038f5c06dbc7d05af222c717d8e9e772b5
Milolikaxx <nusitday106773@gmail.com>
2024-06-26 22:46:30 +0700

    detl check error

--- lib/pages/welcome.dart
+++ lib/pages/welcome.dart
@@ -115,28 +115,7 @@ class _WelcomePageState extends State<WelcomePage> {
                     ],
                   ),
                 ),
-                ElevatedButton(
-                  onPressed: () async {
-                    await GetStorage().erase();
-                    log(GetStorage().read('user'));
-                  },
-                  style: ButtonStyle(
-                    minimumSize:
-                        MaterialStateProperty.all<Size>(const Size(300, 50)),
-                    backgroundColor: MaterialStateProperty.all<Color>(
-                        const Color(0xFFF8721D)),
-                    // textStyle: MaterialStateProperty.all(TextStyle(fontSize: 18, color: Colors.white)),
-                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                      RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(18),
-                      ),
-                    ),
-                  ),
-                  child: const Text(
-                    'ลบ',
-                    style: TextStyle(fontSize: 16, color: Colors.white),
-                  ),
-                ),
+               
               ],
             ),
           ],
