f741a9400ce75fdcef23fb15d4256560f425a9fd
Milolikaxx <nusitday106773@gmail.com>
2024-07-10 14:31:10 +0700

    fix save pl

--- lib/service/provider/appdata.dart
+++ lib/service/provider/appdata.dart
@@ -12,8 +12,8 @@ import 'package:frontend_fitfit_app/service/api/workout_profile.dart';
 
 class AppData with ChangeNotifier {
   //Api baseurl
-  String baseUrl = "http://202.28.34.197:8020";
-  // String baseUrl = "http://192.168.1.24:8080";
+  // String baseUrl = "http://202.28.34.197:8020";
+  String baseUrl = "http://172.31.99.34:8080";
   // String baseUrl = "http://172.20.10.8:8080";
 
   late UserLoginPostResponse user = UserLoginPostResponse();
