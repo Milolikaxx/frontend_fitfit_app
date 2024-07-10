e65e503d56f6218276cb3fb3919407ae1e27f125
TonyTeam007 <64011212102@msu.ac.th>
2024-07-10 23:44:54 +0700

    .

--- lib/service/provider/appdata.dart
+++ lib/service/provider/appdata.dart
@@ -13,13 +13,7 @@ import 'package:frontend_fitfit_app/service/api/workout_profile.dart';
 class AppData with ChangeNotifier {
   //Api baseurl
   // String baseUrl = "http://202.28.34.197:8020";
-  String baseUrl = "http://192.168.101.130:8080";
-<<<<<<< HEAD
-  // String baseUrl = "http://172.20.10.8:8080";
-  String baseUrl = "http://172.16.1.98:8080";
   // String baseUrl = "http://172.31.99.34:8080";
-=======
->>>>>>> 48abc6d (EditPlaylistMusicPage playlist(pid))
   // String baseUrl = "http://172.20.10.8:8080";
   String baseUrl = "http://172.16.1.98:8080";
 
